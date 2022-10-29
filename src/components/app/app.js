67b87f5e1f364f9afb3b430d8bf3a67adf3f5e3a
Spaun1 <andreispaun7@gmail.com>
2022-10-29 09:48:45 +0300

    add search-panel bar

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,5 @@
-import AppInfo from "../app-info/app-info";
+import AppInfo from '../app-info/app-info';
+import SearchPanel from '../search-panel/search-panel';
 
 import './app.css';
 
@@ -6,8 +7,11 @@ function App() {
   return (
     <div className="app">
       <AppInfo />
+      <div className="search-panel">
+        <SearchPanel />
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
