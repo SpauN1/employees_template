df6489eb1ed5bc802ddab3bc1fd1b2550be6ce84
Spaun1 <andreispaun7@gmail.com>
2022-11-26 18:24:09 +0300

    feat: add employee search

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -17,6 +17,7 @@ class App extends Component {
         { name: 'Михаил Т', salary: 800, increase: true, rise: false, id: 2 },
         { name: 'Иван В.', salary: 1100, increase: false, rise: false, id: 3 },
       ],
+      term: '',
     };
     this.maxId = 4;
   }
@@ -56,18 +57,34 @@ class App extends Component {
     }));
   };
 
+  searchEmployees = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter((item) => {
+      return item.name.indexOf(term) > -1;
+    });
+  };
+
+  onUpdateSearch = (term) => {
+    this.setState({term: term});
+  };
+
   render() {
+    const { data, term } = this.state;
     const employees = this.state.data.length;
     const increased = this.state.data.filter((item) => item.increase).length;
+    const visibleData = this.searchEmployees(data, term);
     return (
       <div className="app">
         <AppInfo employees={employees} increased={increased} />
         <div className="search-panel">
-          <SearchPanel />
+          <SearchPanel onUpdateSearch={this.onUpdateSearch} />
           <AppFilter />
         </div>
         <EmployeesList
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}
         />
